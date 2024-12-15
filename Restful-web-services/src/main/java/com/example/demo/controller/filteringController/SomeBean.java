971d5b6605d88f7623c8f41bd2b11d2066b95185
durgesh-work <durgesh.bhalerao42@gmail.com>
2024-12-15 11:04:43 +0530

    static filtering to ignore class member in Json (Method level)

---
+++ Restful-web-services/src/main/java/com/example/demo/controller/filteringController/SomeBean.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller.filteringController;
+
+import org.hibernate.annotations.Comment;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SomeBean {
+
+	@JsonIgnore
+	private String field1;
+	private String field2;
+	private String field3;
+	@Override
+	public String toString() {
+		return "SomeBean [field1=" + field1 + ", field2=" + field2 + ", field3=" + field3 + "]";
+	}
+	
+	
+}
