1f43d31ebe09f8c7c122ab1dae1fdc7c7a0a5975
durgesh-work <durgesh.bhalerao42@gmail.com>
2024-12-17 22:58:06 +0530

    get current port number

--- Currency-Exchange-Microservice/src/main/java/com/currency/exchange/controller/CurrencyExchangeController.java
+++ Currency-Exchange-Microservice/src/main/java/com/currency/exchange/controller/CurrencyExchangeController.java
@@ -1,9 +1,11 @@
 package com.currency.exchange.controller;
 
+import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,9 @@ import com.currency.exchange.entity.CurrencyExchange;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
 
 	@Autowired
 	private ConfigurationForProperties configurationForProperties;
@@ -26,5 +31,11 @@ public class CurrencyExchangeController {
 		return map;
 	}
 	
-	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to) {
+		CurrencyExchange currencyExchange= CurrencyExchange.builder().id(1000L).from(from).to(to).conversionMultiple(BigDecimal.valueOf(50)).build();
+		currencyExchange.setEnvironment( environment.getProperty("local.server.port"));
+		
+		return currencyExchange;
+	}
 }
