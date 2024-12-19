96b1cbcbdfb6db0610915c46ac2b5c4f0a3545b1
durgesh-work <durgesh.bhalerao42@gmail.com>
2024-12-19 15:43:18 +0530

    intial commit

---
+++ Eureka-Naming-Registry-Service/src/main/java/com/naming_service/EurekaNamingRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.naming_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaNamingRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingRegistryServiceApplication.class, args);
+	}
+
+}
